bda8aa170e781038fb59acee325bf6f5c0a4585e
Nicolay Poymanov <n.poymanov@gmail.com>
2018-08-29 23:41:28 +0300

    add app logic

--- README.md
+++ README.md
@@ -1,7 +1,42 @@
 # Night Owls Detector
 
-[TODO. There will be project description]
+Скрипт получает список пользователей [DEVMAN.org](https://devman.org), которые отправляли задания на проверку после полуночи.
 
-# Project Goals
+# Предварительные настройки
 
-The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
+- Установить и запустить [virtualenv/venv](https://devman.org/encyclopedia/pip/pip_virtualenv/) для Python
+- Установить дополнительные пакеты:
+```
+pip install -r requirements.txt
+```
+
+# Как запустить
+
+Скрипт требует для своей работы установленного интерпретатора **Python** версии **3.5**.
+
+**Запуск на Linux**
+
+```bash
+$ python seek_dev_nighters.py # или python3, в зависимости от настроек системы
+
+# результат выполнения скрипта
+user1
+26-05-2018 00:35
+
+user2
+22-07-2018 03:38
+23-08-2018 02:59
+
+# в случае ошибки запроса/обработки данных
+Failed to load attemps info
+
+# в случае если пользователей-полуночников нет
+There are not any midnighters
+
+```
+
+Запуск на **Windows** происходит аналогично.
+
+# Цели проекта
+
+Код создан в учебных целях. В рамках учебного курса по веб-разработке - [DEVMAN.org](https://devman.org)
--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,82 @@
-def load_attempts():
-    pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+import requests
+import json
+import sys
+import pytz
+from datetime import datetime
 
-def get_midnighters():
-    pass
+
+def get_request_url():
+    return 'https://devman.org/api/challenges/solution_attempts'
+
+
+def load_data(request):
+    try:
+        return json.loads(request.text)
+    except json.decoder.JSONDecodeError:
+        return None
+
+
+def get_attemps_info():
+    url = get_request_url()
+    request_success_status = 200
+    attempts_info = []
+    page = 1
+
+    while True:
+        params = {'page': page}
+        request = requests.get(url, params=params)
+        if not request.status_code == request_success_status:
+            return None
+
+        json_data = load_data(request)
+
+        if json_data is None:
+            return None
+
+        attempts_info += json_data['records']
+
+        if not page < json_data['number_of_pages']:
+            break
+
+        page += 1
+
+    return attempts_info
+
+
+def get_midnighters_info(attempts_info):
+    midnighters_info = {}
+
+    for attempt in attempts_info:
+        timezone = pytz.timezone(attempt['timezone'])
+        date = datetime.fromtimestamp(attempt['timestamp'], tz=timezone)
+
+        midnight_from = date.replace(hour=0, minute=0, second=0, microsecond=0)
+        midnight_to = date.replace(hour=4, minute=0, second=0, microsecond=0)
+
+        if midnight_from <= date <= midnight_to:
+            date_format = date.strftime('%d-%m-%Y %H:%M')
+            username = attempt['username']
+            midnighters_info.setdefault(username, []).append(date_format)
+
+    return midnighters_info
+
+
+def output_midnighters_to_console(midnighters_info):
+    print('Midnighters log:', '\n')
+    for midnighter, dates in midnighters_info.items():
+        print(midnighter)
+        print('\n'.join(dates), '\n')
+
 
 if __name__ == '__main__':
-  pass
+    attempts_info = get_attemps_info()
+
+    if not attempts_info:
+        sys.exit('Failed to load attemps info')
+
+    midnighters_info = get_midnighters_info(attempts_info)
+
+    if not midnighters_info:
+        sys.exit('There are not any midnighters')
+
+    output_midnighters_to_console(midnighters_info)
